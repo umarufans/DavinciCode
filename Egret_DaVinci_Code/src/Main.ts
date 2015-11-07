71bd8498f172e28f4005498dd5ee818fb2222c36
Lawrence Lee <596701600@qq.com>
2015-11-07 15:29:32 +0800

    changeStructure

--- Egret_DaVinci_Code/src/Main.ts
+++ Egret_DaVinci_Code/src/Main.ts
@@ -27,6 +27,7 @@
 //
 //////////////////////////////////////////////////////////////////////////////////////
 
+import HorizontalAlign = egret.HorizontalAlign;
 class Main extends egret.DisplayObjectContainer {
 
     /**
@@ -107,51 +108,24 @@ class Main extends egret.DisplayObjectContainer {
      * Create a game scene
      */
     private createGameScene():void {
-        var sky:egret.Bitmap = this.createBitmapByName("bgImage");
-        this.addChild(sky);
-        var stageW:number = this.stage.stageWidth;
-        var stageH:number = this.stage.stageHeight;
-        sky.width = stageW;
-        sky.height = stageH;
-
-        var topMask:egret.Shape = new egret.Shape();
-        topMask.graphics.beginFill(0x000000, 0.5);
-        topMask.graphics.drawRect(0, 0, stageW, stageH);
-        topMask.graphics.endFill();
-        topMask.width = stageW;
-        topMask.height = stageH;
-        this.addChild(topMask);
-
-        var icon:egret.Bitmap = this.createBitmapByName("egretIcon");
-        this.addChild(icon);
-        icon.scaleX = 0.55;
-        icon.scaleY = 0.55;
-        icon.anchorOffsetX = icon.width / 2;
-        icon.anchorOffsetY = icon.height / 2;
-        icon.x = stageW / 2;
-        icon.y = stageH / 2 - 60;
-
-        var colorLabel:egret.TextField = new egret.TextField();
-        colorLabel.textColor = 0xffffff;
-        colorLabel.textAlign = "center";
-        colorLabel.text = "Hello Egret";
-        colorLabel.size = 20;
-        colorLabel.x = stageW - colorLabel.width >> 1;
-        colorLabel.y = (stageH - colorLabel.height >> 1) + 50;
-        this.addChild(colorLabel);
-
-        var textfield:egret.TextField = new egret.TextField();
-        this.addChild(textfield);
-        textfield.alpha = 0;
-        textfield.width = stageW;
-        textfield.textAlign = egret.HorizontalAlign.CENTER;
-        textfield.x = 0;
-        textfield.y = stageH / 2 + 100;
-        this.textfield = textfield;
-
-        //根据name关键字，异步获取一个json配置文件，name属性请参考resources/resource.json配置文件的内容。
-        // Get asynchronously a json configuration file according to name keyword. As for the property of name please refer to the configuration file of resources/resource.json.
-        RES.getResAsync("description", this.startAnimation, this)
+        var background:egret.Shape = new egret.Shape;
+        background.graphics.beginFill(0x00ff00);
+        background.graphics.drawRect(0, 0, this.stage.stageWidth, this.stage.stageHeight);
+        background.graphics.endFill();
+        this.addChild(background);
+        var text:egret.TextField = new egret.TextField;
+        text.text = "Hello World";
+        text.width = this.stage.stageWidth;
+        text.$setTextColor(0x000000);
+        text.$setTextAlign(HorizontalAlign.CENTER);
+        text.touchEnabled = true;
+        text.addEventListener(egret.TouchEvent.TOUCH_TAP, this.touchHandler, this);
+        this.addChild(text);
+    }
+    private touchHandler(event:egret.TouchEvent) {
+        var text = egret.TextField = event.currentTarget;
+        text.textColor = 0xff0000;
+        text.text = "Don't touch me!";
     }
 
     /**
